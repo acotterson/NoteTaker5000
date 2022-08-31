0207e7b5010512b2a70048b30aea5355a238cb52
SalTMongoose <acotterson@users.noreply.github.com>
2022-08-31 10:28:53 -0600

    some minor fixes

--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,8 @@
-const notes = require('exptress').Router();
+const notes = require('express').Router();
 const {readFromFile, readAndAppend} = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
-notes.get('/', req, res => {
+notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(clog);
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", api);
