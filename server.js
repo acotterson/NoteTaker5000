341dd1a8e4d9080fd6abb4b06c0a01af5b278eda
SalTMongoose <acotterson@users.noreply.github.com>
2022-08-30 20:02:48 -0600

    formatting

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const path = require('path');
-const api = require('./routes/index.js');
+const express = require("express");
+const path = require("path");
+const api = require("./routes/index.js");
 
 const PORT = process.env.PORT || 3001;
 
@@ -9,23 +9,23 @@ const app = express();
 app.use(clog);
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use('/api',api);
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", api);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/notes', (req, res) => 
-res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-app.get('/api/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/db/db.json'))
+app.get("/api/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/db/db.json"))
 );
 
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
 app.listen(PORT, () =>
-    console.log(`App listening at http://localhost:${PORT}`)
+  console.log(`App listening at http://localhost:${PORT}`)
 );
